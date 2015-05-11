0c799208030e267ae365f65397fd993d7d421fb6
Shane1993 <513199010@qq.com>
2015-05-11 21:53:30 +0800

    Modify the way collecting the ap informations

--- WiFiLocation2/app/src/main/java/com/example/lenovo/wifilocation/MeasureActivity.java
+++ WiFiLocation2/app/src/main/java/com/example/lenovo/wifilocation/MeasureActivity.java
@@ -19,37 +19,40 @@ import net.lee.wifilocation.config.Config;
 import net.lee.wifilocation.config.MyActionBarConfig;
 import net.lee.wifilocation.model.LocationInfo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import cn.bmob.v3.listener.SaveListener;
 
 /**
  * Created by lenovo on 2015/4/18.
  */
-public class MeasureActivity extends Activity implements View.OnClickListener{
+public class MeasureActivity extends Activity implements View.OnClickListener {
 
-    private ImageButton scanAPBtn,getJSBtn,measureCancleBtn;
-    private TextView locationTv, measureInfoTv,scanAPTv;
+    private ImageButton scanAPBtn, getJSBtn, measureCancleBtn;
+    private TextView locationTv, measureInfoTv, scanAPTv;
     private EditText measureEt;
 
     private LocationInfo locationInfo;
-    private LocationInfo locationInfoFinal;
+    private ArrayList<LocationInfo> locationInfoFinalList;
     //LocationInfo Array for calculate the final LocationInfo
-    private LocationInfo[] locationInfos = new LocationInfo[10];
+    private LocationInfo[] locationInfos = new LocationInfo[20];
 
     private ProgressBar measurePB;
 
-    private int recordCount = 0 ;
+    private int recordCount = 0;
 
     private String areaName;
 
-    private boolean compareResult = true;
+    private int isSuccess;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.measureactivity);
 
-        new MyActionBarConfig(getActionBar(),null);
+        new MyActionBarConfig(getActionBar(), null);
 
         scanAPBtn = (ImageButton) findViewById(R.id.scanAPBtn);
         locationTv = (TextView) findViewById(R.id.apTv);
@@ -72,8 +75,7 @@ public class MeasureActivity extends Activity implements View.OnClickListener{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home)
-        {
+        if (item.getItemId() == android.R.id.home) {
             finish();
         }
 
@@ -94,12 +96,10 @@ public class MeasureActivity extends Activity implements View.OnClickListener{
                 toast("Wifi正在打开中，请稍后再试");
             } else if (MainActivity.wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
 
-                if(!scanAPTv.getText().toString().equals("重新扫描：")) {
+                if (!scanAPTv.getText().toString().equals("重新扫描：")) {
                     //Invoke the Handler's handleMessage
                     timerHandler.sendEmptyMessage(0);
-                }
-                else
-                {
+                } else {
                     recordCount = 0;
                     locationTv.setText("");
                     timerHandler.sendEmptyMessage(0);
@@ -108,112 +108,145 @@ public class MeasureActivity extends Activity implements View.OnClickListener{
 
         } else if (v.getId() == R.id.getJSBtn) {
 
-            if(locationInfoFinal == null || !compareResult)
-            {
+            if (locationInfoFinalList == null) {
                 toast("请定位成功后再上传");
-            }
-            else if(TextUtils.isEmpty(measureEt.getText().toString()))
-            {
+            } else if (TextUtils.isEmpty(measureEt.getText().toString())) {
                 toast("请输入区域名称");
-             }
-            else
-            {
-                //Set the location name
-                locationInfoFinal.setLocationName(measureEt.getText().toString());
-                locationInfoFinal.save(MeasureActivity.this, new SaveListener() {
-                    @Override
-                    public void onSuccess() {
+            } else {
+                isSuccess = 0;
+                for (LocationInfo locationInfoFinal : locationInfoFinalList) {
+                    //Set the location name
+                    locationInfoFinal.setLocationName(measureEt.getText().toString());
+                    locationInfoFinal.save(MeasureActivity.this, new SaveListener() {
+                        @Override
+                        public void onSuccess() {
+                            isSuccess++;
+                            if(isSuccess == locationInfoFinalList.size())
+                            {
+                                toast("上传位置信息成功");
+                                //Close the activity
+                                finish();
+                            }
+                        }
 
-                        toast("上传位置信息成功");
-                        //Close the activity
-                        finish();
-                    }
-
-                    @Override
-                    public void onFailure(int i, String s) {
-                        toast("上传位置信息失败 : " + s);
-                    }
-                });
-
+                        @Override
+                        public void onFailure(int i, String s) {
+                            toast("上传位置信息失败 : " + s);
+                        }
+                    });
+//                    System.out.println("Before sleep");
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    System.out.println("after sleep");
+                }
 
             }
-        } else if (v.getId() == R.id.measureCancleBtn)
-        {
+        } else if (v.getId() == R.id.measureCancleBtn) {
             finish();
         }
     }
 
+    private Map<String, ArrayList<LocationInfo>> locationMap = new HashMap<String, ArrayList<LocationInfo>>();
+
     /**
      * Use of Handler for doing the timecount work(定时任务)
      */
-    private Handler timerHandler = new Handler()
-    {
-        String tempMacSort = "";
+    private Handler timerHandler = new Handler() {
+        //        String tempMacSort = "";
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
 
-            //Only need 10 groups of data
-            if(recordCount < 10) {
+            //Only need 20 groups of data
+            if (recordCount < 20) {
 
                 locationInfo = MainActivity.getLocationInfo();
 
-                if(locationInfo == null)
-                {
-                    toast("当前AP数量过少，无法定位");
-                    //Clear the message
-                    timerHandler.removeMessages(0);
-                    recordCount = 0;
-                    measurePB.setVisibility(View.GONE);
-                    locationTv.setText("当前AP数量过少，无法定位");
-                    return;
+                if (locationInfo == null) {
+//                    toast("当前AP数量过少，无法定位");
+//                    //Clear the message
+//                    timerHandler.removeMessages(0);
+//                    recordCount = 0;
+//                    measurePB.setVisibility(View.GONE);
+//                    locationTv.setText("当前AP数量过少，无法定位");
+//                    return;
+                    locationTv.append("当前AP数量过少，无法定位\n\n");
+                    recordCount++;
+                    //Every 2 second invoke this function
+                    timerHandler.sendEmptyMessageDelayed(0, 2000);
                 }
 
-                scanAPTv.setText("扫描中...");
-                measureInfoTv.setText("正在获取位置信息\n请耐心等候...");
-                measurePB.setVisibility(View.VISIBLE);
-                measurePB.setProgress((int) (10 * (recordCount + 0.5)));
+                if (recordCount == 0) {
+                    scanAPTv.setText("扫描中...");
+                    measureInfoTv.setText("正在获取位置信息\n请耐心等候...");
+                    measurePB.setVisibility(View.VISIBLE);
+                }
+                measurePB.setProgress((int) (5 * (recordCount + 0.5)));
 
-                locationInfos[recordCount] = locationInfo;
+                boolean hasSameKay = false;
+
+                if (locationMap.keySet().size() > 0) {
+                    for (String macSort : locationMap.keySet()) {
+                        if (locationInfo.getMacSort().equals(macSort)) {
+                            ArrayList<LocationInfo> list = locationMap.get(macSort);
+                            list.add(locationInfo);
+                            locationMap.put(macSort, list);
+                            hasSameKay = true;
+                            break;
+                        }
+                    }
+                    //If there are not the same macSort in map, add mew data
+                    if (!hasSameKay) {
+                        ArrayList<LocationInfo> list = new ArrayList<LocationInfo>();
+                        list.add(locationInfo);
+                        locationMap.put(locationInfo.getMacSort(), list);
+                    }
+                } else {
+                    ArrayList<LocationInfo> list = new ArrayList<LocationInfo>();
+                    list.add(locationInfo);
+                    locationMap.put(locationInfo.getMacSort(), list);
+                }
+
+//                locationInfos[recordCount] = locationInfo;
                 locationTv.append(recordCount + 1 + "\nMac1 : " + locationInfo.getMac1() + ", RSSI:" + locationInfo.getMac1Rssi()
                         + "\nMac2 : " + locationInfo.getMac2() + ", RSSI:" + locationInfo.getMac2Rssi()
                         + "\nMac3 : " + locationInfo.getMac3() + ", RSSI:" + locationInfo.getMac3Rssi()
                         + "\nMacSort : " + locationInfo.getMacSort()
                         + "\n\n");
 
-                if(recordCount == 0)
-                {
-                    tempMacSort = locationInfo.getMacSort();
-                    System.out.println("compare1:\n" + tempMacSort);
-                }
-                else
-                {
-                    if(!tempMacSort.equals(locationInfo.getMacSort()))
-                    {
-                        System.out.println("compare2:\n" + tempMacSort + "\n" + locationInfo.getMacSort());
-
-                        compareResult = false;
-                        //Stop invoke this function
-                        timerHandler.removeMessages(0);
-                        recordCount = 0;
-                        measurePB.setVisibility(View.GONE);
-                        measureInfoTv.setText("由于扫描AP存在不同，建议重新扫描");
-                        scanAPTv.setText("重新扫描：");
-                        toast("由于扫描AP存在不同，建议重新扫描");
-
-                        return;
-                    }
-                }
+//                if(recordCount == 0)
+//                {
+//                    tempMacSort = locationInfo.getMacSort();
+//                    System.out.println("compare1:\n" + tempMacSort);
+//                }
+//                else
+//                {
+//                    if(!tempMacSort.equals(locationInfo.getMacSort()))
+//                    {
+//                        System.out.println("compare2:\n" + tempMacSort + "\n" + locationInfo.getMacSort());
+//
+//                        //Stop invoke this function
+//                        timerHandler.removeMessages(0);
+//                        recordCount = 0;
+//                        measurePB.setVisibility(View.GONE);
+//                        measureInfoTv.setText("由于扫描AP存在不同，建议重新扫描");
+//                        scanAPTv.setText("重新扫描：");
+//                        toast("由于扫描AP存在不同，建议重新扫描");
+//
+//                        return;
+//                    }
+//                }
 
                 recordCount++;
                 //Every 2 second invoke this function
                 timerHandler.sendEmptyMessageDelayed(0, 2000);
-                Log.i("Handler", "Send the message");
 
             }
-            else
-            {
-                compareResult = true;
+            //Scan over
+            else {
                 scanAPTv.setText("重新扫描：");
                 //Stop invoke this function
                 timerHandler.removeMessages(0);
@@ -221,10 +254,12 @@ public class MeasureActivity extends Activity implements View.OnClickListener{
                 //Clear the progressbar
                 measurePB.setVisibility(View.GONE);
 
-                locationInfoFinal = getFinalLocation(locationInfos);
-                locationInfoFinal.setAreaName(areaName);
-                measureInfoTv.setText(locationInfoFinal.toString());
-
+                //Calculate the average
+//                locationInfoFinal = getFinalLocationList(locationInfos);
+//                locationInfoFinal.setAreaName(areaName);
+//                measureInfoTv.setText(locationInfoFinal.toString());
+                locationInfoFinalList = getFinalLocationList(locationMap);
+                measureInfoTv.setText(locationInfoFinalList.toString());
 
                 //Invalid to invoke this fuction because of recordcount
                 recordCount++;
@@ -234,49 +269,45 @@ public class MeasureActivity extends Activity implements View.OnClickListener{
 
     /**
      * Get the stable location infomation and return
+     *
      * @return LocationInfo
      */
-    private LocationInfo getFinalLocation(LocationInfo[] locationInfoArray) {
+    private ArrayList<LocationInfo> getFinalLocationList(Map<String, ArrayList<LocationInfo>> map) {
 
-        LocationInfo li = new LocationInfo();
-        int rssi1Final,rssi2Final,rssi3Final;
-        int count = locationInfoArray.length;
-        int sum = 0;
+        ArrayList<LocationInfo> list = new ArrayList<LocationInfo>();
 
-        //Calculate the rssi1Final
-        for(int j = 0; j < count; j++)
-        {
-            sum += locationInfoArray[j].getMac1Rssi();
+        for (ArrayList<LocationInfo> arrayList : map.values()) {
+
+            LocationInfo li = new LocationInfo();
+            int rssi1Final, rssi2Final, rssi3Final;
+            int count = arrayList.size();
+            int sum1 = 0, sum2 = 0, sum3 = 0;
+
+            for (LocationInfo locationInfo : arrayList) {
+                sum1 += locationInfo.getMac1Rssi();
+                sum2 += locationInfo.getMac2Rssi();
+                sum3 += locationInfo.getMac3Rssi();
+            }
+            rssi1Final = sum1 / count;
+            rssi2Final = sum2 / count;
+            rssi3Final = sum3 / count;
+
+            li.setMac1(arrayList.get(0).getMac1());
+            li.setMac2(arrayList.get(0).getMac2());
+            li.setMac3(arrayList.get(0).getMac3());
+            li.setMacSort(arrayList.get(0).getMacSort());
+            li.setMac1Rssi(rssi1Final);
+            li.setMac2Rssi(rssi2Final);
+            li.setMac3Rssi(rssi3Final);
+            li.setAreaName(areaName);
+
+            list.add(li);
         }
-        rssi1Final = sum/count;
-        sum = 0;
 
-        //Calculate the rssi2Final
-        for(int j = 0; j < count; j++)
-        {
-            sum += locationInfoArray[j].getMac2Rssi();
-        }
-        rssi2Final = sum/count;
-        sum = 0;
+        System.out.println(list);
+        return list;
 
-        //Calculate the rssi3Final
-        for(int j = 0; j < count; j++)
-        {
-            sum += locationInfoArray[j].getMac3Rssi();
-        }
-        rssi3Final = sum/count;
-
-        //Set the Mac information, I select the second data(locationInfoArray[1]) for model
-            //Do not ask me why, I just think that's stabler
-        li.setMac1(locationInfoArray[1].getMac1());
-        li.setMac2(locationInfoArray[1].getMac2());
-        li.setMac3(locationInfoArray[1].getMac3());
-        li.setMacSort(locationInfoArray[1].getMacSort());
-        li.setMac1Rssi(rssi1Final);
-        li.setMac2Rssi(rssi2Final);
-        li.setMac3Rssi(rssi3Final);
-
-        return li;
     }
+
 
 }
